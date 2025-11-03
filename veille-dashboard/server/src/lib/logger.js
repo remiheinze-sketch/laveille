672b347bce9aa4f18c7b736701b540ef31bc63fd
remiheinze-sketch <remiheinze@gmail.com>
2025-11-03 14:10:15 +0100

    Add files via upload

---
+++ veille-dashboard/server/src/lib/logger.js
@@ -0,0 +1,3 @@
+export function log(...args){ console.log('[server]', ...args); }
+export function warn(...args){ console.warn('[server]', ...args); }
+export function error(...args){ console.error('[server]', ...args); }
